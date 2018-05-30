2392dac0b22add9a6ba22c99dacbc4bc0c22065f
LuChengTHU <c-lu15@mails.tsinghua.edu.cn>
2018-05-30 17:10:05 +0800

    Add 64

--- models/models.py
+++ models/models.py
@@ -60,25 +60,29 @@ class _netG(nn.Module):
         super(_netG, self).__init__()
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=True),
+            nn.ConvTranspose2d(nz, ngf * 16, 4, 1, 0, bias=True),
+            nn.BatchNorm2d(ngf * 16),
+            nn.ReLU(True),
+            # state size. (ngf*16) x 4 x 4
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=True),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
+            # state size. (ngf*8) x 8 x 8
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=True),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
+            # state size. (ngf*4) x 16 x 16
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=True),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
+            # state size. (ngf * 2) x 32 x 32
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=True),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
+            # state size. (ngf) x 64 x 64
             nn.ConvTranspose2d( ngf, nc, 3, 1, 1, bias=True),
             nn.Tanh()
-            # state size. (nc) x 32 x 32
+            # state size. (nc) x 64 x 64
         )
 
     def forward(self, input):
@@ -90,33 +94,43 @@ class _netD(nn.Module):
         super(_netD, self).__init__()
 
         self.main = nn.Sequential(
-            # input is (nc) x 32 x 32
+            # input is (nc) x 64 x 64
             #SNConv2d()
             SNConv2d(nc, ndf, 3, 1, 1, bias=True),
             nn.LeakyReLU(0.1, inplace=True),
             SNConv2d(ndf, ndf, 4, 2, 1, bias=True),
             nn.LeakyReLU(0.1, inplace=True),
-            # state size. (ndf) x 1 x 32
+            # state size. (ndf) x 1 x 64
+
             SNConv2d(ndf, ndf * 2, 3, 1, 1, bias=True),
             nn.LeakyReLU(0.1, inplace=True),
             SNConv2d(ndf * 2, ndf * 2, 4, 2, 1, bias=True),
             #nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.1, inplace=True),
-            # state size. (ndf*2) x 16 x 16
+            # state size. (ndf*2) x 32 x 32
+
             SNConv2d(ndf * 2, ndf * 4, 3, 1, 1, bias=True),
             nn.LeakyReLU(0.1, inplace=True),
             SNConv2d(ndf * 4, ndf * 4, 4, 2, 1, bias=True),
             #nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.1, inplace=True),
-            # state size. (ndf*4) x 8 x 8
+            # state size. (ndf*4) x 16 x 16
+
             SNConv2d(ndf * 4, ndf * 8, 3, 1, 1, bias=True),
+            nn.LeakyReLU(0.1, inplace=True),
+            SNConv2d(ndf * 8, ndf * 8, 4, 2, 1, bias=True),
             #nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.1, inplace=True),
-            # state size. (ndf*8) x 4 x 4
+            # state size. (ndf*8) x 8 x 8
+
+            SNConv2d(ndf * 8, ndf * 16, 3, 1, 1, bias=True),
+            #nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.1, inplace=True),
+            # state size. (ndf*16) x 4 x 4
             #SNConv2d(ndf * 8, 1, 4, 1, 0, bias=False),
             #nn.Sigmoid()
         )
-        self.snlinear = SNLinear(ndf * 8 * 4 * 4, 1)
+        self.snlinear = SNLinear(ndf * 16 * 4 * 4, 1)
 
     def forward(self, input):
         output = self.main(input)
